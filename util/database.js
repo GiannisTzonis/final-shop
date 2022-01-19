4eac7cf6385643a1fee50029481e0c4d9b739950
GiannisTzonis <tzonis.john@hotmail.com>
2022-01-19 17:49:14 +0200

    connected mongodb

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://tzon1s:jZuAaIU1p3CW3SOd@cluster0.zrsgo65.mongodb.net/?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected!");
