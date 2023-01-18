a94caed12e5ca80e71ec2c9ab07a114cff038a04
GiannisTzonis <tzonis.john@hotmail.com>
2023-01-18 07:16:36 +0200

    added request login in

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
+  User.findById("63cf244f0910abb7ca5aae07")
     .then((user) => {
       req.user = user;
       next();
