d89d2d8cd8a35f2be54bb0a9d61d5a1058f6a6e1
GiannisTzonis <tzonis.john@hotmail.com>
2023-01-18 07:44:32 +0200

    added session to store data

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://tzon1s:fYBwyDgyZHPhfuMJ@cluster0.zrsgo65.mongodb.net/shop?retryWrites=true&w=majority";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,9 +27,20 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById("63cf244f0910abb7ca5aae07")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -33,12 +53,9 @@ app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
-mongoose.set("strictQuery", false);
 
 mongoose
-  .connect(
-    "mongodb+srv://tzon1s:fYBwyDgyZHPhfuMJ@cluster0.zrsgo65.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
